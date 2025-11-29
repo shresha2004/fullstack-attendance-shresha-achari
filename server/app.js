82d4aef4c892c173e605d622cbee6e816258d584
Shresha Acharya <shreshacharya06@gmail.com>
2025-11-29 18:06:21 +0530

    Client deployed url pasted to server for CORS verification

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 // Middleware
 const corsOptions = {
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: 'https://fullstack-attendance-shresha-achari.vercel.app/',
   credentials: true
 };
 
