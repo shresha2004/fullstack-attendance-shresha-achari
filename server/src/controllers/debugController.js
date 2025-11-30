df7c019a2aa6dd12cd82799ec3205f9298171d80
Shresha Acharya <shreshacharya06@gmail.com>
2025-11-30 21:13:05 +0530

    Files and folders configured

--- server/src/controllers/debugController.js
+++
@@ -1,9 +0,0 @@
-export const getDebug = (req, res) => {
-  // Return helpful debug info about the authenticated request
-  const authHeader = req.headers.authorization || null;
-  res.json({
-    message: 'debug info',
-    authHeader,
-    user: req.user || null,
-  });
-};
